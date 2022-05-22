439afd826a99bd8873529394bedf6bc1565da318
DEYA <widianathadeya@student.telkomuniversity.ac.id>
2022-05-22 20:46:37 +0700

    assessment2

---
+++ app/src/main/java/org/d3if0028/hitungnilaiakhir/model/InputUser.kt
@@ -0,0 +1,9 @@
+package org.d3if0028.hitungnilaiakhir.model
+
+data class InputUser(
+    var nama: String,
+    var hadir : Float,
+    var tugas : Float,
+    var uts : Float,
+    var uas : Float
+)
