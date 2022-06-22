ef7c635f3ad6b29d144ea0b172b3d3dd9103d85c
DEYA <widianathadeya@student.telkomuniversity.ac.id>
2022-06-22 13:11:39 +0700

    asessment 3

--- app/src/main/java/org/d3if0028/hitungnilaiakhir/MainActivity.kt
+++ app/src/main/java/org/d3if0028/hitungnilaiakhir/MainActivity.kt
@@ -1,14 +1,23 @@
 package org.d3if0028.hitungnilaiakhir
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import org.d3if0028.hitungnilaiakhir.ui.saran.SaranFragment.Companion.CHANNEL_ID
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+
+    companion object{
+        const val CHANNEL_ID = "updater"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +25,16 @@ class MainActivity : AppCompatActivity() {
 
         navController = findNavController(R.id.NavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = getString(R.string.channel_desc)
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE)
+                    as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/org/d3if0028/hitungnilaiakhir/model/Saran.kt
@@ -0,0 +1,9 @@
+package org.d3if0028.hitungnilaiakhir.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Saran(
+    @SerializedName("data")
+    val `data`: List<DataSaran?>?
+)
